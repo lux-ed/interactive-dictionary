41cca1da1c968d44eadbc2447ce482fd7b2ecbfb
lux-ed <lux-ed@users.noreply.github.com>
2018-11-25 00:02:58 +0100

    existing file

---
+++ app.py
@@ -0,0 +1,30 @@
+import json
+from difflib import get_close_matches
+
+data = json.load(open("data.json"))
+
+def translate(w):
+    w = w.lower()
+    if w in data:   #if returns true
+        return data[w]
+    elif len(get_close_matches(w, data.keys())) > 0:
+        yn = input("Did you mean %s instead? Enter Y is yes, or N if no: "%get_close_matches(w, data.keys())[0])
+        if yn == 'Y':
+            return data[get_close_matches(w, data.keys())[0]]
+        elif yn == 'N':
+            return "the word doesn't exist. Please double check it."
+        else:
+            return "We didn't understand your query. Please double check it."
+    else:        
+        return " \n The word doesn't exist. Please double check it. "
+
+word = input("Enter word: ")   
+#global variable, we define it here
+
+output = translate(word)   
+#global variable, and then we pass the variable here. Whatever is passed in translate(whatever is passed), goes back to the function argument.
+if type(output) == list:
+    for item in output:
+        print("\n", item)
+else:
+    print(output)
